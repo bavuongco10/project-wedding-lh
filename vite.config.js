2cac7d27c9d53ba4e16e09183a698b21463932d9
buithanhbavuong <bavuongco10@gmail.com>
2025-12-29 18:06:26 +0700

    Optimize images at build time

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 75,
+      },
+      jpg: {
+        quality: 75,
+      },
+      webp: {
+        quality: 80,
+      },
+      avif: {
+        quality: 70,
+      },
+      svg: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+          {
+            name: 'sortAttrs',
+            active: true,
+          },
+        ],
+      },
+    }),
     {
       name: 'html-transform',
       transformIndexHtml(html) {
